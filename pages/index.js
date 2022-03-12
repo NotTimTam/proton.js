a79b5e304d0d182422ce28a3d91ee1dd316ed9b7
NotTimTam <Tphill180@west-mec.local>
2022-03-11 17:56:00 -0700

    Added cards.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Pagination, ProgressBar } from "../proton.js";
+import { Button, Card, Pagination, ProgressBar } from "../proton.js";
 import { BsToggle2Off, BsToggle2On } from "react-icons/bs";
 
 export default function Home() {
@@ -10,13 +10,120 @@ export default function Home() {
 	};
 
 	return (
-		<div
+		<Card.Group
 			style={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				justifyContent: "center",
+				height: "90vh",
 			}}
-		></div>
+		>
+			<Card
+				color="red"
+				hovereffect
+				noborder
+				style={{ maxWidth: "20rem" }}
+			>
+				<Card.Image>
+					<img
+						src="https://picsum.photos/1000/800"
+						style={{ width: "100%", height: "200px" }}
+					/>
+				</Card.Image>
+				{/* <Card.Header>
+					<h1>Hello World</h1>
+				</Card.Header> */}
+				<Card.Content flex>
+					<h3>Duis id Cillum</h3>
+					Anim duis laborum ullamco cillum qui amet consequat nulla
+					culpa amet incididunt sint ex quis.
+				</Card.Content>
+			</Card>
+			<Card
+				color="red"
+				hovereffect
+				dropshadow
+				noborder
+				style={{ maxWidth: "20rem" }}
+			>
+				<Card.Image>
+					<img
+						src="https://picsum.photos/1001/800"
+						style={{ width: "100%", height: "200px" }}
+					/>
+				</Card.Image>
+				{/* <Card.Header>
+					<h1>Hello World</h1>
+				</Card.Header> */}
+				<Card.Content flex>
+					<h3>Duis id Cillum</h3>
+					Anim duis laborum ullamco cillum qui amet consequat nulla
+					culpa amet incididunt sint ex quis.
+				</Card.Content>
+			</Card>
+			<Card
+				color="red"
+				hovereffect
+				dropshadow
+				noborder
+				style={{ maxWidth: "20rem" }}
+			>
+				<Card.Image>
+					<img
+						src="https://picsum.photos/1002/800"
+						style={{ width: "100%", height: "200px" }}
+					/>
+				</Card.Image>
+				{/* <Card.Header>
+					<h1>Hello World</h1>
+				</Card.Header> */}
+				<Card.Content flex>
+					<h3>Duis id Cillum</h3>
+					Anim duis laborum ullamco cillum qui amet consequat nulla
+					culpa amet incididunt sint ex quis.
+				</Card.Content>
+			</Card>
+			<Card
+				color="red"
+				hovereffect
+				dropshadow
+				noborder
+				style={{ maxWidth: "20rem" }}
+			>
+				<Card.Image>
+					<img
+						src="https://picsum.photos/1003/800"
+						style={{ width: "100%", height: "200px" }}
+					/>
+				</Card.Image>
+				{/* <Card.Header>
+					<h1>Hello World</h1>
+				</Card.Header> */}
+				<Card.Content flex>
+					<h3>Duis id Cillum</h3>
+					Anim duis laborum ullamco cillum qui amet consequat nulla
+					culpa amet incididunt sint ex quis.
+				</Card.Content>
+			</Card>
+			<Card
+				color="red"
+				hovereffect
+				dropshadow
+				noborder
+				style={{ maxWidth: "20rem" }}
+			>
+				<Card.Image>
+					<img
+						src="https://picsum.photos/1004/800"
+						style={{ width: "100%", height: "200px" }}
+					/>
+				</Card.Image>
+				{/* <Card.Header>
+					<h1>Hello World</h1>
+				</Card.Header> */}
+				<Card.Content flex>
+					<h3>Duis id Cillum</h3>
+					Anim duis laborum ullamco cillum qui amet consequat nulla
+					culpa amet incididunt sint ex quis.
+				</Card.Content>
+			</Card>
+		</Card.Group>
 	);
 }
--- proton.js/index.js
+++ proton.js/index.js
@@ -1,5 +1,6 @@
 import Button from "./Button/Button";
 import ProgressBar from "./Progress/Progress";
 import Pagination from "./Pagination/Pagination";
+import Card from "./Card/Card";
 
-export { Button, ProgressBar, Pagination };
+export { Button, ProgressBar, Pagination, Card };
